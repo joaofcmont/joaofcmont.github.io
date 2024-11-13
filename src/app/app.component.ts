caac6f3172d9e5c765c548f5be37e4385a8f640f
joaomonteiro <jfcmo98647@gmail.com>
2024-11-13 19:22:19 +0000

    v1.0 portfolio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { HomeComponent } from "./home/home.component";
+import { ProjectComponent } from "./project/project.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, HomeComponent, ProjectComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']  
 })
 export class AppComponent {
   title = 'portfolio';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { ProjectComponent } from './project/project.component';
+import { BlogComponent } from './blog/blog.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'project', component: ProjectComponent},
+    { path: '', redirectTo: '', pathMatch: 'full' },
+    { path: 'blog', component: BlogComponent},
+
+];
